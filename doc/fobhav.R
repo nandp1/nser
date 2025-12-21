287258c02269edd4c9444bbba3160833f1b6294b
nandp1 <tryanother609@gmail.com>
2025-12-21 20:46:47 +0530

    Add files via upload

--- doc/fobhav.R
+++ doc/fobhav.R
@@ -1,12 +1,6 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
-
-## ----example------------------------------------------------------------------
-# Example: Lets download the bhavcopy of 1 July 2021 for Derivatives (F&O). 
-library(nser)
-report = fobhav("01072021")
-head(report)
 
